bb3203cef81a827e365deb9cfcd1a0f6db53d0ae
David Ensor <DavidEnsor5000@gmail.com>
2023-02-11 08:11:47 -0700

    Muzia v1.01 visible keys resolved

--- Routes/googleAuth.js
+++ Routes/googleAuth.js
@@ -1,40 +1,41 @@
-require ('dotenv').config();
+
 const passport = require('passport');
+require('dotenv')?.config();
 
 
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 passport.use('google', new GoogleStrategy({
-    clientID: "290860385790-ol28keolhg8jua5u864on9o7vv0a44jv.apps.googleusercontent.com",
-    clientSecret: "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",
+    clientID: `${process?.env?.GOOGLE_clientID}`,
+    clientSecret: `${process?.env?.GOOGLE_clientSecret}`,
     callbackURL: "/google/callback"
-  },
+},
 
-  function(accessToken, refreshToken, profile, cb) {
-    try {
-        db.collection("users")?.findOne({googleId : profile?.id}).then((response)=>{
-            if(response===null || response===undefined){
-                db.collection('users')?.insertOne({googleId : profile?.id, fullname : profile?._json?.name, profilePicture: profile._json?.picture, email: profile?._json?.email, stagename: profile?.displayName, gender:profile?.gender, websiteCreated:"" }).then(()=>{
+    function (accessToken, refreshToken, profile, cb) {
+        try {
+            db.collection("users")?.findOne({ googleId: profile?.id }).then((response) => {
+                if (response === null || response === undefined) {
+                    db.collection('users')?.insertOne({ googleId: profile?.id, fullname: profile?._json?.name, profilePicture: profile._json?.picture, email: profile?._json?.email, stagename: profile?.displayName, gender: profile?.gender, websiteCreated: "" }).then(() => {
+                        cb(null, profile);
+                    })
+
+                } else {
                     cb(null, profile);
-                })
-    
-            }else{
-                cb(null, profile);
-            }
-        })
-    } catch (error) {
-        console.log(error);
-        cb(error, false)
+                }
+            })
+        } catch (error) {
+            console.log(error);
+            cb(error, false)
+        }
     }
-  }
 ));
 
-passport?.serializeUser((user, done)=>{
-   
+passport?.serializeUser((user, done) => {
+
     try {
-        if(user?.id){
+        if (user?.id) {
             done(null, user?.id)
-        }else if(user?._id){
+        } else if (user?._id) {
             done(null, user?._id)
         }
     } catch (error) {
@@ -43,17 +44,17 @@ passport?.serializeUser((user, done)=>{
     }
 })
 
-passport.deserializeUser((user,done)=>{
+passport.deserializeUser((user, done) => {
 
-   try {
-        if(user?.id){
+    try {
+        if (user?.id) {
             done(null, user?.id)
-        }else if(user?._id){
+        } else if (user?._id) {
             done(null, user?._id)
         }
-   } catch (error) {
+    } catch (error) {
         console.log(error)
         res?.sendStatus(500)
-   }
+    }
 })
 
