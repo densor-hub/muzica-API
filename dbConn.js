aa666c3e2bbce7949a6021250fd9b0f49e6e8e84
David Ensor <DavidEnsor5000@gmail.com>
2023-05-07 15:39:56 -0700

    serving frontend via backend

--- dbConn.js
+++ dbConn.js
@@ -9,7 +9,7 @@ let dbConnection;
 module.exports = {
     connectToDb: ((callBack) => {
         try {
-            MongoClient.connect(localCluster)
+            MongoClient.connect(onlineCluster)
                 .then((client) => {
                     dbConnection = client.db();
                     console.log('connected to database')
